19c69a76b02a4397c244b11f66b4609fe00cf613
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-06-20 16:09:23 +0700

    Add database model

---
+++ backend/models/todo.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+	todo_description: String,
+	todo_responsible: String,
+	todo_priority: String,
+	todo_completed: Boolean
+});
+
+const Todo = mongoose.model('Todo', TodoSchema);
+
+module.exports = Todo;
