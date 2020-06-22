4576129fe4ce44c14f1b9196c933bc3c37ddddcf
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-06-22 11:11:21 +0700

    Add real bug fix

--- backend/models/todo.model.js
+++ backend/models/todo.model.js
@@ -10,17 +10,5 @@ const TodoSchema = new mongoose.Schema({
 const Todo = mongoose.model('Todo', TodoSchema);
 
 module.exports = Todo;
-//Fix:
-//module.exports = mongoose => { return Todo };
 
-/*module.exports = mongoose => {
-	const Todo = mongoose.model("Todo", mongoose.Schema({
-		todo_description: String,
-		todo_responsible: String,
-		todo_priority: String,
-		todo_completed: Boolean
-	})
-	);
 
-	return Todo
-};*/
